d3f337ffb344259942a6eb9de9dc530ec8b56372
eastmarch <29557084+eastmarch@users.noreply.github.com>
2020-04-17 23:35:05 -0400

    first

---
+++ background.js
@@ -0,0 +1,32 @@
+'use strict';
+
+chrome.runtime.onInstalled.addListener(function() {
+  chrome.contextMenus.create({
+    id: "play-in-mpv",
+    title: "Play link in MPV",
+    contexts: ["link"],
+  });
+});
+
+chrome.contextMenus.onClicked.addListener(function(info, tab) {
+  if (info.menuItemId == "play-in-mpv") playInMPV(info.linkUrl);
+});
+
+chrome.browserAction.onClicked.addListener(function(tab) {
+  chrome.tabs.executeScript({code: `(${String(pausePlayback)})();`});
+  playInMPV(tab.url);
+});
+
+function playInMPV(targetURL) {
+  var link = new URL(targetURL);
+  link.protocol = "ytdl:";
+  // chrome.tabs.update({ url: link.href });
+  chrome.tabs.executeScript({code: `window.location.href = "${link.href}";`});
+}
+
+function pausePlayback() {
+  var htmlPlayer = document.querySelector("video");
+  if(htmlPlayer != null) {
+    htmlPlayer.pause();
+  }
+}
